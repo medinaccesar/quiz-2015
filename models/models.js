70d16b6703dce84c3be5ce434ce5fee6f586914a
CésarM <medinaccesar@gmail.com>
2015-08-02 13:36:08 +0200

    Lista de Preguntas

--- models/models.js
+++ models/models.js
@@ -41,9 +41,19 @@ sequelize.sync().then(function() {
   // then(..) ejecuta el manejador una vez creada la tabla
   Quiz.count().then(function (count){
     if(count === 0) {   // la tabla se inicializa solo si está vacía
-      Quiz.create({ pregunta: 'Capital de Italia',
-      	            respuesta: 'Roma'
-      	         })
+    //Se pueden añadir preguntas y respuestas por partes para inicializar la tabla	
+      // Quiz.create({ pregunta: 'Capital de Italia',
+      // 	            respuesta: 'Roma'
+      // 	         });
+      // Quiz.create({ pregunta: 'Capital de Portugal',
+      // 	            respuesta: 'Lisboa'
+      // 	         })
+      // o mejor hacerlo añadirlas en bloque
+      Quiz.bulkCreate( 
+        [ {pregunta: 'Capital de Italia',   respuesta: 'Roma'},
+          {pregunta: 'Capital de Portugal', respuesta: 'Lisboa'}
+        ]
+      )
       .then(function(){console.log('Base de datos inicializada')});
     };
   });
