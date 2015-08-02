debad2b64236b22e129a2bda7222df5f10b33a3f
CésarM <medinaccesar@gmail.com>
2015-08-02 22:29:43 +0200

    Rama busquedas ej Mod 7

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -13,8 +13,18 @@ exports.load = function(req, res, next, quizId) {
 };
 // GET /quizes
 exports.index = function(req, res) {
-  models.Quiz.findAll().then(
-    function(quizes) {
+	console.log(req.query);
+  if(req.query.search){  	  
+  	  var busqueda = req.query.search.split(' ').join('%');
+  		models.Quiz.findAll({where: ["pregunta like ?", '%'+busqueda+'%']}).then(
+  			function(quizes) {
+		      res.render('quizes/index', { quizes: quizes});
+		    }
+  		).catch(function(error) { next(error);});
+
+  }else		
+  models.Quiz.findAll().then(  	
+    function(quizes) {    	
       res.render('quizes/index', { quizes: quizes});
     }
   ).catch(function(error) { next(error);});
