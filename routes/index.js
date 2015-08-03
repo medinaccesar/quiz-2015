dbf0de7a8e04c21ccb31461c3a0ef28fe549886b
CésarM <medinaccesar@gmail.com>
2015-08-03 14:35:05 +0200

    Permitir borrar una pregunta

--- routes/index.js
+++ routes/index.js
@@ -27,6 +27,8 @@ router.post('/quizes/create',              quizController.create);
 router.get('/quizes/:quizId(\\d+)/edit',   quizController.edit);
 router.put('/quizes/:quizId(\\d+)',        quizController.update);
 
+router.delete('/quizes/:quizId(\\d+)',     quizController.destroy);
+
 router.get('/authors',   quizController.authors);//+++
 
 module.exports = router;
