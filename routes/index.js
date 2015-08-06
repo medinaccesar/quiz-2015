819367503fbdcedb29b35b4c0abd08e59c2ca47f
CésarM <medinaccesar@gmail.com>
2015-08-06 19:04:25 +0200

    Moderación de comentarios

--- routes/index.js
+++ routes/index.js
@@ -13,6 +13,7 @@ router.get('/', function(req, res) {
 
 // Autoload de comandos con :quizId
 router.param('quizId', quizController.load);  // autoload :quizId
+router.param('commentId', commentController.load);  // autoload :commentId
 
 // Definición de rutas de sesion
 router.get('/login',  sessionController.new);     // formulario login
@@ -39,6 +40,7 @@ router.delete('/quizes/:quizId(\\d+)',     sessionController.loginRequired,quizC
 
 router.get('/quizes/:quizId(\\d+)/comments/new',            commentController.new);
 router.post('/quizes/:quizId(\\d+)/comments',              commentController.create);
+router.get('/quizes/:quizId(\\d+)/comments/:commentId(\\d+)/publish',  sessionController.loginRequired, commentController.publish);
 
 router.get('/authors',   quizController.authors);//+++
 
