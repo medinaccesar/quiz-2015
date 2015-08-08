4d11e37b0e44151b96b3bc26624a6334ecf2d654
CésarM <medinaccesar@gmail.com>
2015-08-08 12:48:23 +0200

    Ej. Mod 9. Autodesconexión si han transcurrido más de 2 min desde la última transacción, con comentarios adicionales

--- app.js
+++ app.js
@@ -44,6 +44,31 @@ if (!req.path.match(/\/login|\/logout/)) {
   next();
 });
 
+//MW Se comprueba la sesión a fin de desconectar si han transcurrido más de dos min
+app.use(function(req, res, next){
+
+    if(req.session.user){ //si hay sesión
+        var ahora_ml = new Date().getTime();//marca temporal actual en milisegundos
+        var dos_minutos_ml = 120000;//2*60*1000 dos minutos en milisegundos
+
+        if(req.session.user.tiempo){ // si está establecida la marca temporal «tiempo»     
+            //si han pasado más de 120 segundos redireccionamos para destruir la sesión
+            if(ahora_ml - res.locals.session.user.tiempo  > dos_minutos_ml){                
+                res.redirect('/logout');
+                //en lugar de esto, se podría haber borrado 
+                //la sesión directamente y redireccionado a login
+                //delete req.session.user;
+                //res.redirect('/login');
+            }
+
+        }else{            
+            req.session.user.tiempo = ahora_ml; //se asigna la marca temporal «tiempo»
+        }     
+    } 
+    next();
+});
+
+
 
 app.use('/', routes);
 //app.use('/users', users);//---
